e0fe3b79a26b10475d3466b69942857688d199fc
sstampher <stephanierstampher@gmail.com>
2019-07-31 15:42:42 -0700

    completed basic beverages model

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,41 @@
 const db = require('./connection');
 
+const Beverage = db.define('beverage', {
+  id: {
+    primaryKey: true,
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  manufacturer: {
+    type: Sequelize.STRING
+  },
+  percentAlcohol: {
+    type: Sequelize.INTEGER
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  category: {
+    type: Sequelize.ENUM([])
+  },
+  price: {
+    type: Sequelize.INTEGER
+  },
+  size: {
+    type: Sequelize.INTEGER
+  },
+  quantity: {
+    type: Sequelize.INTEGER
+  },
+  imageURL: {
+    type: Sequelize.STRING
+  }
+})
+
 module.exports = {
   db,
+  Beverage
 };
