b8155bee2d2068e42b061891e3426a4a4cdab196
Justin Cook <justin.cook.dev@gmail.com>
2019-07-31 09:44:49 -0700

    Basic express app. NPM scripts.

---
+++ server/app/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+
+const staticPath = path.join(__dirname, '..', '..', 'public');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(staticPath));
+
+app.get('hello', (req, res) => res.send('hi!'));
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const app = require('./app/index');
+const { db } = require('./db/index');
+
+const PORT = process.env.PORT || 3000;
+
+db.sync()
+  .then(() => {
+    app.listen(PORT, () => console.log(`App listening at port: ${PORT}`));
+  })
+  .catch(console.error);
