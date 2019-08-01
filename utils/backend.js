7779ca8aef9b899f84790b2e924c7900675d0640
Justin Cook <justin.cook.dev@gmail.com>
2019-08-01 11:39:30 -0700

    Added default arguments to authError function in utils/backend.

--- utils/backend.js
+++ utils/backend.js
@@ -1,5 +1,5 @@
 // Designed for use in user model
-export const authError = (msg, subtype) => {
+export const authError = (msg = 'Error', subtype = 'Error') => {
   const error = new Error(msg);
   error.type = 'Auth';
   error.subtype = subtype;
