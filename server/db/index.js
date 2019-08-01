e30784b3279663c1e6843d0e84b9f184a54addff
sstampher <stephanierstampher@gmail.com>
2019-07-31 17:48:03 -0700

    added details to Beverages model

--- server/db/index.js
+++ server/db/index.js
@@ -8,31 +8,40 @@ const Beverage = db.define('beverage', {
     defaultValue: Sequelize.UUIDV4
   },
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    notEmpty: true,
+    unique: true
   },
   manufacturer: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    notEmpty: true
   },
   percentAlcohol: {
-    type: Sequelize.INTEGER
+    type: Sequelize.FLOAT,
+    allowNull: false
   },
   description: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    notEmpty: true
   },
   category: {
     type: Sequelize.ENUM(['category1','category2', 'category3'])
   },
   price: {
-    type: Sequelize.INTEGER
+    type: Sequelize.FLOAT,
+    allowNull: false
   },
   size: {
-    type: Sequelize.INTEGER
+    type: Sequelize.FLOAT,
+    allowNull: false
   },
   quantity: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
+    allowNull: false
   },
   imageURL: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    defaultValue: 'https://brouwlandprod-yappa.netdna-ssl.com/image/fancy/products-20151110012036-0175273.jpg'
   }
 })
 
