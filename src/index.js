2f1067fa90367e3d15e513d5e51c946922d021c1
Justin Cook <justin.cook.dev@gmail.com>
2019-08-06 11:31:35 -0700

    Changed import order.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-
 import store from './store';
 
 ReactDOM.render(
--- src/store.js
+++ src/store.js
@@ -3,13 +3,13 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-const middleware = [ thunk ];
+const middleware = [thunk];
 
 const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware( ...middleware )
-)
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
 
 console.log('store: ', store.getState());
 
