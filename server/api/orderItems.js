a60034a474ae1efee0be08aaf0789edba615edcd
Justin Cook <justin.cook.dev@gmail.com>
2019-08-10 08:45:28 -0700

    Refactored orderItem api routes.

--- server/api/orderItems.js
+++ server/api/orderItems.js
@@ -1,34 +1,34 @@
 const router = require('express').Router();
-const { Order, OrderItem } = require('../db/index');
+const { Order, OrderItem, Beverage } = require('../db/index');
 
-// get the order id for a user
+// make updates to other models... Bev (quantity)
+
+const itemExistsMiddleware = (req, res, next) => {
+  if (!req.item) {
+    res.sendStatus(400);
+  } else {
+    next();
+  }
+};
+
 router.use(async (req, res, next) => {
-  const userId = req.user.id;
-  const [order] = await Order.findOrCreate({
-    where: { userId, purchased: false },
-  });
-  req.orderId = order.id;
-  next();
-});
-
-// make payload
-router.use((req, res, next) => {
-  if (req.method === 'POST' || req.method === 'PUT') {
-    req.payload = {
-      beverageId: req.body.beverageId,
-      purchasePrice: req.body.purchasePrice,
-      quantity: req.body.quantity,
-      orderId: req.orderId,
-    };
+  if (
+    req.method === 'POST' ||
+    (req.method === 'GET' && req.route.path === '/api/orderItems/')
+  ) {
+    const userId = req.user.id;
+    const [order] = await Order.findOrCreate({
+      where: { userId, purchased: false },
+    });
+    req.orderId = order.id;
   }
   next();
 });
 
 router.get('/', async (req, res, next) => {
   try {
-    const { orderId } = req;
     const items = await OrderItem.findAll({
-      where: { orderId },
+      where: { orderId: req.orderId },
     });
     res.json(items);
   } catch (error) {
@@ -38,10 +38,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const { orderId } = req;
-    const item = await OrderItem.findOne({
-      where: { orderId, id: req.params.id },
-    });
+    const item = await OrderItem.findOne({ where: { id: req.params.id } });
     res.json(item);
   } catch (error) {
     next(error);
@@ -50,11 +47,9 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    const { payload } = req.payload;
-    const { orderId, beverageId, purchasePrice, quantity } = payload;
-
+    const { beverageId, purchasePrice, quantity } = req.body;
     const [item, created] = await OrderItem.findOrCreate({
-      where: { orderId, beverageId },
+      where: { orderId: req.orderId, beverageId },
       defaults: { purchasePrice, quantity },
     });
 
@@ -68,53 +63,32 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.param('beverageId', async (req, res, next, beverageId) => {
+router.param('id', async (req, res, next, id) => {
   try {
-    const { orderId } = req;
-    const item = await OrderItem.findOne({ where: { orderId, beverageId } });
+    const item = await OrderItem.findOne({ where: { id } });
     req.item = item ? item : null;
+    next();
   } catch (error) {
     next(error);
   }
 });
 
-router.put(
-  '/:beverageId',
-  (req, res, next) => {
-    if (!req.item) {
-      res.sendStatus(400);
-    } else {
-      next();
-    }
-  },
-  async (req, res, next) => {
-    try {
-      const { quantity } = req.payload;
-      const updatedItem = await req.item.update({ quantity });
-      res.json(updatedItem);
-    } catch (error) {
-      next(error);
-    }
+router.put('/:id', itemExistsMiddleware, async (req, res, next) => {
+  try {
+    const updatedItem = await req.item.update({ quantity: req.body.quantity });
+    res.json(updatedItem);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
-router.delete(
-  '/:beverageId',
-  (req, res, next) => {
-    if (!req.item) {
-      res.sendStatus(400);
-    } else {
-      next();
-    }
-  },
-  async (req, res, next) => {
-    try {
-      await req.item.destroy();
-      res.sendStatus(204);
-    } catch (error) {
-      next(error);
-    }
+router.delete('/:id', itemExistsMiddleware, async (req, res, next) => {
+  try {
+    await req.item.destroy();
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 module.exports = router;
