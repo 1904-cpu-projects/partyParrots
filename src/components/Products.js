687ae8f1637eefcd482321a2c2e82ab4e28c31b1
Justin Cook <justin.cook.dev@gmail.com>
2019-08-13 09:24:11 -0700

    Removed fetch products on products page mount.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,39 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import ProductCardSmall from './ProductCardSmall'
+import ProductCardSmall from './ProductCardSmall';
 import { fetchAllBeverages } from '../actions/beverageActions';
 
 class Products extends Component {
-
-    componentDidMount(){
-      this.props.fetchBeverages();
-    }
-
-    render(){
-      return  <section className="section" style={{paddingTop:'120px'}}>
-                <div className="container">
-                  <div className = "columns is-centered is-multiline">
-                    {this.props.beverages.map( 
-                      beverage => <div key={beverage.id} className = "column is-2">< ProductCardSmall  beverage = { beverage } /></div>
-                    )}
-                  </div>
-                </div>
-              </section>
-    }
-
+  render() {
+    return (
+      <section className="section" style={{ paddingTop: '120px' }}>
+        <div className="container">
+          <div className="columns is-centered is-multiline">
+            {this.props.beverages.map(beverage => (
+              <div key={beverage.id} className="column is-2">
+                <ProductCardSmall beverage={beverage} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
-const mapStateToProps = state => (
-{
-    beverages: state.beverages
-})
+const mapStateToProps = state => ({
+  beverages: state.beverages,
+});
 
-const mapDispatchToProps = ( dispatch ) => {
-    return {
-        fetchBeverages: () => {
-            dispatch( fetchAllBeverages() )
-        }
-    }
-}
-
-export default connect( mapStateToProps , mapDispatchToProps )( Products );
+export default connect(
+  mapStateToProps,
+  null
+)(Products);
