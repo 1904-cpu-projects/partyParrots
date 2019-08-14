d3a1eba4a4a76d18ad9736ef6a4f0a925103b229
Justin Cook <justin.cook.dev@gmail.com>
2019-08-13 21:40:18 -0700

    Changed order item styling. Fixed floating point issue.

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { isLoggedInMiddleware } = require('../../utils/backend');
 
 router.use('/beverages', require('./beverages'));
-router.use('/orderItems', isLoggedInMiddleware, require('./orderItems'));
+router.use('/orderItems', require('./orderItems'));
 router.use('/users', require('./users'))
 
 module.exports = router;
--- server/app/index.js
+++ server/app/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const compression = require('compression');
 const helmet = require('helmet');
 const sessionMiddleware = require('./sessionMiddlewarare');
+const sessiodIdMiddleware = require('./sessionIdMiddleware')
 const { serializeUserMiddleware } = require('./serializeUserMiddleware');
 
 const staticPath = path.join(__dirname, '..', '..', 'public');
@@ -13,6 +14,7 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(compression());
 app.use(sessionMiddleware);
+app.use(sessiodIdMiddleware)
 app.use(serializeUserMiddleware);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
