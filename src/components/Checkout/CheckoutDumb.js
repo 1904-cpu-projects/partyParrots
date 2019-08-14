0ebfe29fa4004c93da60a8b1c58bb844133e23eb
ElectricZebra <nicholasregoli@gmail.com>
2019-08-14 12:56:19 -0700

    create basic Dumb component

---
+++ src/components/Checkout/CheckoutDumb.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Checkout extends Component {
+  constructor () {
+    super();
+    this.state = {
+      values: {
+
+      },
+      errors: {},
+    }
+  }
+
+  handleChange(){
+
+  }
+
+  handleSubmit(){
+
+  }
+
+  clear() {
+
+  }
+
+  render() {
+
+    return(
+      <div></div>
+    )
+  }
+}
