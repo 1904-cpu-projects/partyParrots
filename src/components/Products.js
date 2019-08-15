68ce7541851299b20beb221eb803d26f7a2768ce
sstampher <stephanierstampher@gmail.com>
2019-08-15 02:49:26 -0700

    added products title and an hr for prof

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import ProductCardSmall from './ProductCardSmall';
+import React from "react";
+import { connect } from "react-redux";
+import ProductCardSmall from "./ProductCardSmall";
 
 function Products(props) {
   return (
-    <section className="section" style={{ paddingTop: '175px' }}>
+    <section className="section" style={{ paddingTop: "90px" }}>
+      <div style={{ marginBottom: "50px" }}>
+        <p className="center" style={{ fontSize: "45px" }}>
+          Products
+        </p>
+        <hr />
+      </div>
+
       <div className="container">
         <div className="columns is-centered is-multiline">
           {props.beverages.map(beverage => (
@@ -19,7 +26,7 @@ function Products(props) {
 }
 
 const mapStateToProps = state => ({
-  beverages: state.beverages,
+  beverages: state.beverages
 });
 
 export default connect(mapStateToProps)(Products);
