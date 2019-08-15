274bdc640286cf5647fdbc888f8e16a9486c10af
ElectricZebra <nicholasregoli@gmail.com>
2019-08-15 08:01:50 -0700

    submit button render

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,47 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import _Checkout from './CheckoutDumb';
 
-const Checkout = () => {
-  return <hr />
+class Checkout extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      values: {},
+      errors: {},
+    };
+
+  }
+
+  clear = () => {
+    return 'hi'
+  };
+
+  handleChange = () => {
+    return 'hi'
+  };
+
+  handleSubmit = () => {
+    return 'hi'
+  };
+
+  render(){
+    return (
+      <div>
+        <_Checkout />
+      </div>
+    )
+  }
 }
 
-export default Checkout;
 
-// class Checkout extends Component {
-//   constructor () {
-//     super();
-//     this.state = {
-//       values: {
-
-//       },
-//       errors: {},
-//     }
-//   }
-
-//   handleChange(){
-
-//   }
-
-//   handleSubmit(){
-
-//   }
-
-//   clear() {
-
-//   }
-
-//   render() {
-//     return(
-//       <div>
-//         <hr />
-//       </div>
-//     )
-//   }
-// }
-
-// export default Checkout;
-
-// export default connect(
-//   null,
-//   null
-//   )(Checkout);
+export default connect(
+  null,
+  null
+  )(Checkout);
