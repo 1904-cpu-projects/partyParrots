fe96e63cba06d0a8207ab127286d193d56569aad
Justin Cook <justin.cook.dev@gmail.com>
2019-08-15 23:12:41 -0700

    Transactions page! Simple and functional.

---
+++ server/api/orders.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Order, OrderItem, Beverage } = require('../db/index');
+const { isLoggedInMiddleware } = require('../../utils/backend');
+
+router.get('/', isLoggedInMiddleware, async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      where: { userId: req.user.id, purchased: true },
+    });
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id/items', isLoggedInMiddleware, async (req, res, next) => {
+  try {
+    const items = await OrderItem.findAll({
+      where: { orderId: req.params.id },
+      include: [{ model: Beverage }],
+    });
+    res.json(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
