bd5408d373884da1c2a170f08903dd3e2be3f3d6
Justin Cook <justin.cook.dev@gmail.com>
2019-08-18 10:48:15 -0700

    Product modal is now responsive. Works on phones!

--- src/components/DetailedProduct/DetailedProductDumb.js
+++ src/components/DetailedProduct/DetailedProductDumb.js
@@ -1,79 +1,66 @@
-import React from "react";
-import QuantitySelector from "../QuantitySelector/QuantitySelector";
+import React from 'react';
+import QuantitySelector from '../QuantitySelector/QuantitySelector';
 
 export default function DetailedProduct(props) {
   return (
-    <div className={props.modal ? "modal is-active" : "modal"}>
-      <a className="modal-background" onClick={props.handleClick} />
-      <div className="modal-content">
-        <div className="box">
-          <article className="media">
-            <div
-              className="media-left center"
-              style={{
-                height: "370px",
-                width: "300px",
-                marginTop: "20px",
-                marginBottom: "20px",
-                marginRight: "10px",
-                marginLeft: "10px"
-              }}
-            >
-              <img
-                src={props.beverage.imageURL}
-                style={{ height: "410px", width: "325px" }}
-                alt="Image"
-              />
-            </div>
-            <div
-              className="media-content"
-              style={{ marginTop: "50px", marginRight: "50px" }}
-            >
-              <div className="content">
-                <div style={{ paddingBottom: "5px" }}>
-                  <div className="is-size-4 has-text-weight-semibold">
-                    {props.beverage.name}
-                  </div>
-                  <div className="is-italic is-size-6 has-text-weight-medium">
-                    {props.beverage.manufacturer}
-                  </div>
-                </div>
-                <br />
+    <div className={props.modal ? 'modal is-active' : 'modal'}>
+      <div className="modal-background" onClick={props.handleClick} />
 
-                <div style={{ paddingBottom: "5px" }}>
-                  {props.beverage.description}
+      <div className="modal-content box">
+        <article
+          className="media"
+          style={{ display: 'flex', alignItems: 'center' }}
+        >
+          <div className="media-left" style={{ flexBasis: '50%' }}>
+            <p className="image is-4by5">
+              <img src={props.beverage.imageURL} alt="Image" />
+            </p>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <div style={{ paddingBottom: '5px' }}>
+                <div className="is-size-4 has-text-weight-semibold">
+                  {props.beverage.name}
                 </div>
-                <br />
-                <small>
-                  {props.beverage.size + " fl. oz."}
-                  <br />
-                  {"Alcohol by Volume: " + props.beverage.percentAlcohol + "%"}
-                  <br />
-                  {"Category: " + props.beverage.category}
-                </small>
-                <br />
+                <div className="is-italic is-size-6 has-text-weight-medium">
+                  {props.beverage.manufacturer}
+                </div>
               </div>
-              <nav className="level is-mobile">
-                <div className="level-left">
-                  <div className="is-size-5 has-text-danger">
-                    {"$" + props.beverage.price + " ea."}
-                  </div>
-                </div>
-                {props.inCart && props.inCart.id ? (
-                  <div className="level-right control">
-                    <QuantitySelector item={props.inCart} showZero={true} />
-                  </div>
-                ) : (
-                  <a className="level-right button is-text">
-                    <span className="icon is-size-4" onClick={props.addToCart}>
-                      <i className="fas fa-cart-plus" />
-                    </span>
-                  </a>
-                )}
-              </nav>
+              <br />
+
+              <div style={{ paddingBottom: '5px' }}>
+                {props.beverage.description}
+              </div>
+              <br />
+              <small>
+                {props.beverage.size + ' fl. oz.'}
+                <br />
+                {'Alcohol by Volume: ' + props.beverage.percentAlcohol + '%'}
+                <br />
+                {'Category: ' + props.beverage.category}
+              </small>
+              <br />
             </div>
-          </article>
-        </div>
+            <div className="level is-mobile">
+              <div className="level-left">
+                <div className="is-size-5 has-text-danger">
+                  {'$' + props.beverage.price + ' ea.'}
+                </div>
+              </div>
+              {props.inCart && props.inCart.id ? (
+                <div className="level-right control">
+                  <QuantitySelector item={props.inCart} showZero={true} />
+                </div>
+              ) : (
+                <a className="level-right button is-text">
+                  <span className="icon is-size-4" onClick={props.addToCart}>
+                    <i className="fas fa-cart-plus" />
+                  </span>
+                </a>
+              )}
+            </div>
+          </div>
+        </article>
       </div>
     </div>
   );
